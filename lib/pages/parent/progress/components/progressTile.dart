e26c02bfc47b727f839d93db4024a3e937de2651
redthecoderist1000 <redochavillo@gmail.com>
2025-08-14 03:43:44 +0800

    feat: implement ProgressTile widget with item display

--- lib/pages/parent/progress/components/progressTile.dart
+++ lib/pages/parent/progress/components/progressTile.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
 class ProgressTile extends StatelessWidget {
-  const ProgressTile({super.key});
+  final dynamic item;
+
+  const ProgressTile({super.key, this.item});
+
+  get index => null;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      spacing: 30,
+      children: [
+        Text('${index + 1}'),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.only(right: 10),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(
+                  radius: 20,
+                  backgroundColor: Colors.grey[200],
+                  child: Icon(Icons.face_5_rounded, size: 30),
+                ),
+                Text(item['name']),
+                Text(item['score'].toString()),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
